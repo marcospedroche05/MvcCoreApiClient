60cd14bdaf801049d1a28227096b52d999053279
marcospedroche05 <marcospedroche62@gmail.com>
2026-03-25 10:49:02 +0100

    e

---
+++ MvcCoreApiClient/Controllers/HospitalesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreApiClient.Models;
+using MvcCoreApiClient.Services;
+
+namespace MvcCoreApiClient.Controllers
+{
+    public class HospitalesController : Controller
+    {
+        private ServiceHospital service;
+        public HospitalesController(ServiceHospital service)
+        {
+            this.service = service;
+        }
+
+        public async Task<IActionResult> Servidor()
+        {
+            List<Hospital> hospitales = await this.service.GetHospitalsAsync();
+            return View(hospitales);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cliente()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            Hospital hospital = await this.service.FindHospitalAsync(id);
+            return View(hospital);
+        }
+
+
+    }
+}
---
+++ MvcCoreApiClient/Services/ServiceHospital.cs
@@ -0,0 +1,70 @@
+﻿using MvcCoreApiClient.Models;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+namespace MvcCoreApiClient.Services
+{
+    public class ServiceHospital
+    {
+        public string ApiUrl;
+        //NECESITAMOS INDICAR EL TIPO DE DATOS QUE VAMOS A LEER
+        private MediaTypeWithQualityHeaderValue header;
+        public ServiceHospital(IConfiguration configuration)
+        {
+            this.ApiUrl = configuration.GetValue<string>("ApiUrls:ApiHospitales");
+            this.header = new MediaTypeWithQualityHeaderValue("application/json");
+        }
+
+        public async Task<List<Hospital>> GetHospitalsAsync()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string request = "api/hospitales";
+                //INDICAMOS EL HOST
+                client.BaseAddress = new Uri(this.ApiUrl);
+                //INDICAMOS LOS DATOS QUE VAMOS A CONSUMIR
+                //LIMPIAMOS LAS CABECERAS POR NORMA
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.header);
+                //REALIZAMOS LA PETICION Y CAPTURAMOS UNA RESPUESTA
+                HttpResponseMessage response = await client.GetAsync(request);
+                //EN LA RESPUESTA TENEMOS LA CLAVE SI DESEAMOS PERSONALIZAR
+                //ERRORES
+                if(response.IsSuccessStatusCode == true)
+                {
+                    //RECUPERAMOS EL CONTENIDO EN json
+                    string json = await response.Content.ReadAsStringAsync();
+                    //MEDIANTE NEWTON DESERIALIZAMOS JSON A LIST
+                    List<Hospital> hospitales = JsonConvert.DeserializeObject<List<Hospital>>(json);
+                    return hospitales;
+                } else
+                {
+                    return null;
+                }
+            }
+        }
+
+        public async Task<Hospital> FindHospitalAsync(int idHospital)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string request = "api/hospitales/" + idHospital;
+                client.BaseAddress = new Uri(this.ApiUrl);
+                client.DefaultRequestHeaders.Accept.Add(this.header);
+                HttpResponseMessage response = await client.GetAsync(request);
+                if(response.IsSuccessStatusCode == true)
+                {
+                    //SI LAS PROPIEDADES DEL MODEL Y DEL JSON SE LLAMAN IGUAL,
+                    //NO ES NECESARIO DECORAR CON 
+                    //[JsonProperty] Y TAMPOCO UTILIZAR JsonConvert
+                    Hospital hospital =
+                        await response.Content.ReadAsAsync<Hospital>();
+                    return hospital;
+                } else
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
